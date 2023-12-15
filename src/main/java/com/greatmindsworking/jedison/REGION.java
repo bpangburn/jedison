4a208a3c4cb0d98345b047451d671b806f6b88b0
Brian Pangburn <swingset@pangburngroup.com>
2023-12-15 13:56:39 -0600

    Updated copyright through 2024.

--- src/main/java/com/greatmindsworking/jedison/REGION.java
+++ src/main/java/com/greatmindsworking/jedison/REGION.java
@@ -2,7 +2,7 @@
  *
  * Tab Spacing = 4
  *
- * Copyright (c) 2002-2004, Brian E. Pangburn & Jonathan P. Ayo
+ * Copyright (c) 2002-2024, Brian E. Pangburn & Jonathan P. Ayo
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
